448bec0f9b3e6e05b3649726b5a8e1c70217264a
fabien townsend <fabientownsend@gmail.com>
2017-01-17 15:15:02 +0000

    Add feature for the menu
    this add the possibility to update the url of the menu
    in order to let the user update the menu. the class can
    also parse a message to get the url provided by the user.

--- menu.rb
+++ menu.rb
@@ -1,5 +1,15 @@
 class Menu
-  def url 
-    "www.mymenu.com"
+  attr_reader :url
+
+  def initialize
+    @url = "no link provided"
+  end
+
+  def set_url(url)
+    @url = url
+  end
+
+  def parse_url(text)
+    text[/((http|https):\/\/)?(w{3}.)?[A-Za-z0-9-]+.(com|co.uk)/]
   end
 end
--- menu_test.rb
+++ menu_test.rb
@@ -1,9 +1,56 @@
 require_relative 'menu'
 
 RSpec.describe Menu do
-  it "return the url of the current menu" do
+  it "notify if no link provided" do
     menu = Menu.new
-    expect(menu.url).to eq("www.mymenu.com")
+    expect(menu.url).to eq("no link provided")
   end
-  #@menu_url = /((http|https):\/{2})+([A-Za-z]+\.)+(com|co.uk)/.match(event_data['message'])
+
+  it "we can change the default url" do
+    menu = Menu.new
+    menu.set_url("www.newmenu.com")
+    expect(menu.url).to eq("www.newmenu.com")
+  end
+
+  it "return the url without the www" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla http://menu.com asdfad")
+    expect(url).to eq("http://menu.com")
+  end
+
+  it "return the com url" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla http://www.menu.com asdfad")
+    expect(url).to eq("http://www.menu.com")
+  end
+
+  it "return the co.uk url" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla http://www.menu.co.uk asdfad")
+    expect(url).to eq("http://www.menu.co.uk")
+  end
+
+  it "return url with numbers" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla http://www.m1234enu.co.uk asdfad")
+    expect(url).to eq("http://www.m1234enu.co.uk")
+  end
+
+  it "return url with -" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla http://www.m-enu.co.uk asdfad")
+    expect(url).to eq("http://www.m-enu.co.uk")
+  end
+
+  it "return the url without the http" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla www.menu.co.uk asdfad")
+    expect(url).to eq("www.menu.co.uk")
+  end
+
+  it "return the url with https" do
+    menu = Menu.new
+    url = menu.parse_url("asdfj dasfjkla https://www.menu.co.uk asdfad")
+    expect(url).to eq("https://www.menu.co.uk")
+  end
 end
