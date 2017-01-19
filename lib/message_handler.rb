bb047f525362c0f3510d4207985156365e7e7350
fabien townsend <fabientownsend@gmail.com>
2017-01-19 12:13:00 +0000

    Make the bot recognise when the message is new menu
    When the bot recognise the message as a new menu, it display
    a specific message. For that the request parser is use which
    let us determin what kind of message to reply to the user.

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,18 +1,27 @@
 require_relative 'menu'
 require_relative 'response'
+require_relative 'request_parser'
 
 class MessageHandler
   def initialize()
     @menu = Menu.new
+    @request_parser = RequestParser.new
   end
   
   def handle(team_id, event_data)
     user_id = event_data['user']
-    respondToMessage(team_id, user_id)
+
+    if @request_parser.parse(event_data['text']) == "menu"
+      respondToMessage(team_id, user_id, event_data['channel'])
+    else
+      respondToMessage(team_id, user_id)
+    end
+
+
   end
 
-  def respondToMessage(team_id, user_id)
-      response = Response.new(team_id, user_id)
+  def respondToMessage(team_id, user_id, channel = user_id)
+      response = Response.new(team_id, channel)
       response.send("Menu has been set.")
   end
 end
