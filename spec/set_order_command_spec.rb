11400e1e7a27ca70b53642485e5e869f8789223d
fabien townsend <fabientownsend@gmail.com>
2017-01-27 10:43:00 +0000

    Remove duplication in set order tests

--- spec/set_order_command_spec.rb
+++ spec/set_order_command_spec.rb
@@ -1,10 +1,12 @@
 require 'set_order_command'
 
 RSpec.describe SetOrderCommand do
+  let (:event_data_from_will) { {user_id: "asdf", user_name: "Will" } }
+  let (:event_data_from_fabien) { {user_id: "qwert", user_name: "Fabien" } }
+
   it "reccord an order in the database" do
     message = "burger"
-    event_data = {user_id: "asdf", user_name: "a name" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
     set_order_command.run
 
     expect(Order.count).to eq(1)
@@ -12,8 +14,7 @@ RSpec.describe SetOrderCommand do
 
   it "a user should have only one order" do
     message = "burger"
-    event_data = {user_id: "asdf", user_name: "a name" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
 
     set_order_command.run
     set_order_command.run
@@ -23,13 +24,12 @@ RSpec.describe SetOrderCommand do
 
   it "updates users pre existing order when they place another order" do
     message = "chilli"
-    event_data = {user_id: "asdf", user_name: "a name" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
 
     set_order_command.run
 
     message = "burger"
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
     set_order_command.run
 
     expect(Order.last(:user_id => "asdf").lunch).to eq("burger")
@@ -37,12 +37,10 @@ RSpec.describe SetOrderCommand do
 
   it "total of order is equal to two when get two different order" do
     message = "burger"
-    event_data = {user_id: "asdf", user_name: "a name" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
     set_order_command.run
 
-    event_data = {user_id: "qwery", user_name: "a name" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_fabien)
     set_order_command.run
 
     expect(Order.count).to eq(2)
@@ -50,12 +48,10 @@ RSpec.describe SetOrderCommand do
 
   it "return the name of the first persn which ordered" do
     message = "burger"
-    event_data = {user_id: "asdf", user_name: "Will" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_will)
     set_order_command.run
 
-    event_data = {user_id: "qwery", user_name: "Fabien" }
-    set_order_command = SetOrderCommand.new(message, event_data)
+    set_order_command = SetOrderCommand.new(message, event_data_from_fabien)
     set_order_command.run
 
     expect(Order.first.user_name).to eq("Will")
