0cf0c049d7d287472f836867c5dbe4ad111535bc
Will Curry <watercrafthd@gmail.com>
2017-02-01 12:10:42 +0000

    Fix indentation of command info

--- lib/command_info.rb
+++ lib/command_info.rb
@@ -1,32 +1,27 @@
 module CommandInfo
   def all_command_info
-    "To remind everyone who hasn't orders| remind \n"
-    + guest_command_info
-    + foreman_command_info
-    + menu_command_info
+    info_array.join("\n")
   end
 
   private
 
+  def info_array
+    info = [menu_command_info, order_command_info, foreman_command_info, "To remind everyone who hasn't orders| remind"]
+  end
+
   def menu_command_info
-    "To set a menu| new menu www.menu-url.com \n
-    To get the menu| menu? \n"
+    "To set a menu| new menu www.menu-url.com \nTo get the menu| menu?"
   end
 
   def order_command_info
-    "To place an order| order me: food \n
-    To see someone elses order| order @name_of_person \n
-    To see all orders| all orders? \n"
+    "To place an order| order me: food \nTo see someone elses order| order @name_of_person \nTo see all orders| all orders?"
   end
 
   def guest_command_info
-    "To add a guest with no order| add guest: name of guest \n
-    To remove a guest| remove guest: name of guest \n
-    To place an order for a guest (this also creates if the name does not exist)| order -name of guest-: food \n"
+    "To add a guest with no order| add guest: name of guest \nTo remove a guest| remove guest: name of guest \nTo place an order for a guest (this also creates if the name does not exist)| order -name of guest-: food"
   end
 
   def foreman_command_info
-    "To find out the weeks foreman| foreman \n
-    To get a new foreman of the week| next foreman \n"
+    "To find out the weeks foreman| foreman \nTo get a new foreman of the week| next foreman"
   end
 end
