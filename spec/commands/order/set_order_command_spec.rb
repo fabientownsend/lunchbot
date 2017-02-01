63de12bd4760ac7b32c654acb4d7664f5df0d355
fabien townsend <fabientownsend@gmail.com>
2017-02-01 14:57:09 +0000

    User helper to set a order

--- spec/commands/order/set_order_command_spec.rb
+++ spec/commands/order/set_order_command_spec.rb
@@ -1,60 +1,26 @@
 require 'commands/order/set_order_command'
+require 'models/order'
+require 'spec_helper'
 
 RSpec.describe SetOrderCommand do
-  let (:event_data_from_will) { {user_id: "asdf", user_name: "Will", user_message: "fish" } }
-  let (:event_data_from_fabien) { {user_id: "qwert", user_name: "Fabien", user_message: "burger" } }
-
   it "reccord an order in the database" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
-    set_order_command.run
+    Helper.order({user_id: "asdf", user_name: "will", user_message: "burger"})
 
-    expect(Order.count).to eq(1)
+    expect(Order.last(:user_name => "will").lunch).to eq("burger")
   end
 
-  it "a user should have only one order" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
+  it "update the order when the user id already exist" do
+    Helper.order({user_id: "asdf", user_name: "will", user_message: "burger"})
+    Helper.order({user_id: "asdf", user_name: "will", user_message: "fish"})
 
-    set_order_command.run
-    set_order_command.run
-
-    expect(Order.count).to eq(1)
+    expect(Order.last(:user_name => "will").lunch).to eq("fish")
   end
 
-  it "updates users pre existing order when they place another order" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
-    set_order_command.run
+  it "save different order when user id is different" do
+    Helper.order({user_id: "asdf", user_name: "will", user_message: "burger"})
+    Helper.order({user_id: "qwer", user_name: "fabien", user_message: "fish"})
 
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare({user_id: "asdf", user_name: "Will", user_message: "burger"})
-    set_order_command.run
-
-    expect(Order.last(:user_id => "asdf").lunch).to eq("burger")
-  end
-
-  it "total of order is equal to two when get two different order" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
-    set_order_command.run
-
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_fabien)
-    set_order_command.run
-
-    expect(Order.count).to eq(2)
-  end
-
-  it "return the name of the first persn which ordered" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
-    set_order_command.run
-
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_fabien)
-    set_order_command.run
-
-    expect(Order.first.user_name).to eq("Will")
+    expect(Order.last(:user_name => "will").lunch).to eq("burger")
+    expect(Order.last(:user_name => "fabien").lunch).to eq("fish")
   end
 end
