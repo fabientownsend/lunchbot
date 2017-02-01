35782aefa94c73a756b08a1a1c7e964014309059
fabien townsend <fabientownsend@gmail.com>
2017-02-01 13:59:52 +0000

    Remove get all guest dependency to set guest

--- spec/commands/order/get_all_guests_spec.rb
+++ spec/commands/order/get_all_guests_spec.rb
@@ -1,8 +1,7 @@
 require 'commands/order/get_all_guests'
-require 'commands/order/set_order_command'
+require 'spec_helper'
 
 RSpec.describe GetAllGuests do
-  let (:event_data_from_will) { {user_id: "asdf", user_name: "Will", user_message: "burger" } }
   let (:guest_provider) { GetAllGuests.new }
 
   it "return list of guest when only one guest" do
@@ -10,35 +9,24 @@ RSpec.describe GetAllGuests do
   end
 
   it "return list of guest when only one guest" do
-    guest_order_for("james smith")
+    Helper.order_guest({name: "james smith", meal: "burger"})
 
     expect(guest_provider.run).to eq("james smith")
   end
 
   it "return the list of the guests when multiple guests" do
-    guest_order_for("james smith")
-    guest_order_for("jean bon")
+    Helper.order_guest({name: "james smith", meal: "burger"})
+    Helper.order_guest({name: "jean bon", meal: "burger"})
 
     expect(guest_provider.run).to eq("james smith\njean bon")
   end
 
   it "doesn't return the crafters" do
-    set_order_command = SetOrderCommand.new
-    set_order_command.prepare(event_data_from_will)
-    set_order_command.run
-
-    guest_order_for("james smith")
-    guest_order_for("jean bon")
+    Helper.order({user_id: "asdf", user_name: "Will", user_message: "burger"})
+    Helper.order_guest({name: "james smith", meal: "burger"})
+    Helper.order_guest({name: "jean bon", meal: "burger"})
 
     expect(guest_provider.run).to eq("james smith\njean bon")
     expect(Order.last(:user_id => "asdf")).not_to be_nil
-  end
-
-  private
-
-  def guest_order_for(name)
-    place_order_guest = PlaceOrderGuest.new
-    place_order_guest.prepare({user_id: "host id", user_message: "order -#{name}-: burger"})
-    place_order_guest.run
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,12 @@ class Helper
     set_order_command.prepare(data)
     set_order_command.run
   end
+
+  def self.order_guest(data)
+    place_order_guest = PlaceOrderGuest.new
+    place_order_guest.prepare({user_id: "host id", user_message: "order -#{data[:name]}-: #{data[:meal]}"})
+    place_order_guest.run
+  end
 end
 
 RSpec.configure do |config|
