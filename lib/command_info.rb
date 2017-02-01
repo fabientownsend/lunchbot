6c9fe83d049d6c7e8c9a79c50d22fadbb521fbe5
Will Curry <watercrafthd@gmail.com>
2017-02-01 12:16:31 +0000

    Fix typos and format

--- lib/command_info.rb
+++ lib/command_info.rb
@@ -6,22 +6,22 @@ module CommandInfo
   private
 
   def info_array
-    info = [menu_command_info, order_command_info, foreman_command_info, "To remind everyone who hasn't orders| remind"]
+    info = [menu_command_info, order_command_info, foreman_command_info, "To remind everyone who hasn't ordered | remind"]
   end
 
   def menu_command_info
-    "To set a menu| new menu www.menu-url.com \nTo get the menu| menu?"
+    "To set a menu | new menu www.menu-url.com \nTo get this weeks menu | menu?"
   end
 
   def order_command_info
-    "To place an order| order me: food \nTo see someone elses order| order @name_of_person \nTo see all orders| all orders?"
+    "To place an order| order me: food \nTo see someone elses order | order @name_of_person \nTo see all orders | all orders?"
   end
 
   def guest_command_info
-    "To add a guest with no order| add guest: name of guest \nTo remove a guest| remove guest: name of guest \nTo place an order for a guest (this also creates if the name does not exist)| order -name of guest-: food"
+    "To add a guest with no order | add guest: name of guest \nTo remove a guest | remove guest: name of guest \nTo place an order for a guest (this also creates if the name does not exist) | order -name of guest-: food"
   end
 
   def foreman_command_info
-    "To find out the weeks foreman| foreman \nTo get a new foreman of the week| next foreman"
+    "To find out this weeks foreman | foreman \nTo change this weeks foreman to the next in line | next foreman"
   end
 end
