e03f56e17e921cb3175275aefbc7e96a5dff3752
Will Curry <watercrafthd@gmail.com>
2017-02-01 15:36:18 +0000

    Refactor set order command a little

--- lib/commands/order/set_order_command.rb
+++ lib/commands/order/set_order_command.rb
@@ -12,22 +12,32 @@ class SetOrderCommand
     order = Order.last(:user_id => @user_id)
 
     if order
-      order.lunch = @lunch
-      order.save
+      update(order)
     else
-      order = Order.new(
-        :user_name => @user_name,
-        :user_id => @user_id,
-        :lunch => @lunch,
-        :date => Time.now
-      )
-      order.save
+      place_new_order
     end
 
-    "Your order `#{order.lunch}` is updated"
+    "Your order `#{Order.first(:user_id => @user_id).lunch}` is updated"
   end
 
   def applies_to(request)
     request.start_with?("order me: ") && request.split.size > 2
+  end
+
+  private
+
+  def update(order)
+    order.lunch = @lunch
+    order.save
+  end
+
+  def place_new_order
+    order = Order.new(
+      :user_name => @user_name,
+      :user_id => @user_id,
+      :lunch => @lunch,
+      :date => Time.now
+    )
+    order.save
   end
 end
