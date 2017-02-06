b70f71e6bd23362c94d4092e6bb7452263f2978b
fabien townsend <fabientownsend@gmail.com>
2017-02-06 12:59:30 +0000

    Make it use the helper to add a guest

--- spec/commands/order/add_guest_spec.rb
+++ spec/commands/order/add_guest_spec.rb
@@ -5,47 +5,36 @@ RSpec.describe AddGuest do
   let (:event_data_from_will) { {user_id: "asdf", user_name: "Will" } }
 
   it "save a guest" do
-    response = add_guest("james smith")
+    response = Helper.add_guest("james smith")
 
      expect(Order.last(:user_name => "james smith")).not_to be(nil)
      expect(response).to eq("james smith has been added as a guest!")
   end
 
   it "said if a guest already exist" do
-    response = add_guest("james smith")
-    response = add_guest("james smith")
+    response = Helper.add_guest("james smith")
+    response = Helper.add_guest("james smith")
 
     expect(Order.count).to eq(1)
     expect(response).to eq("james smith already exist")
   end
 
   it "add different guest" do
-    add_guest("james smith")
-    add_guest("smith james ")
+    Helper.add_guest("james smith")
+    Helper.add_guest("smith james ")
 
     expect(Order.count).to eq(2)
   end
 
   it "remove the spaces before and after names" do
-    add_guest("  james smith  ")
+    Helper.add_guest("  james smith  ")
 
     expect(Order.last.user_name).to eq("james smith")
   end
 
   it "reccord the name in lowercase" do
-    add_guest("JAMES SMITH")
+    Helper.add_guest("JAMES SMITH")
 
     expect(Order.last.user_name).to eq("james smith")
-  end
-
-  private
-
-  def add_guest(name)
-    place_order_guest = AddGuest.new
-    place_order_guest.prepare({
-      user_message: "add guest: #{name}",
-      user_id: "host id"
-    })
-    place_order_guest.run
   end
 end
