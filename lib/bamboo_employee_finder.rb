826c96c22fb06c457d3627040c9df4cc0fea8e73
Will Curry <watercrafthd@gmail.com>
2017-02-09 11:34:11 +0000

    Clean EmployeeFinder a bit more

--- lib/bamboo_employee_finder.rb
+++ lib/bamboo_employee_finder.rb
@@ -1,5 +1,3 @@
-require 'bamboo_hr'
-
 class BambooEmployeeFinder
   def initialize(channel_users, bamboo_employees)
     @channel_users = channel_users
@@ -7,28 +5,32 @@ class BambooEmployeeFinder
   end
 
   def employee_id(slack_id)
-    return employee_data(slack_id)["employeeId"]
+    return employee_data(slack_id, "employeeId")
   end
 
   private
 
-  def employee_data(slack_id)
+  def employee_data(slack_id, data_wanted)
     @channel_users.each do |user|
       if user[:user_id] == slack_id
-        return employee_data_for(user)
+        return specific_data(user, data_wanted)
       end
+      return "User does not exist in the channel!"
     end
+  end
+
+  def specific_data(user, data_wanted)
+    employee = employee_data_for(user)
+    return employee[data_wanted]
   end
 
   def employee_data_for(user)
     @bamboo_employees.each do |employee|
-      if are_match(user, employee) 
-        return employee
-      end
+      return employee if have_same_email(user, employee) 
     end
   end
 
-  def are_match(user, employee)
+  def have_same_email(user, employee)
     return user[:email] == employee["workEmail"]
   end
 end
