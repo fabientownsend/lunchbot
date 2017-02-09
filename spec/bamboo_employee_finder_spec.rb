b2bfd62a0df6f0491ecb43af95008a36e784fef9
Will Curry <watercrafthd@gmail.com>
2017-02-09 11:34:11 +0000

    Clean EmployeeFinder a bit more

--- spec/bamboo_employee_finder_spec.rb
+++ spec/bamboo_employee_finder_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe BambooEmployeeFinder do
   let (:fake_data_slack) {[{user_id: "id", email: "email"}]}
   let (:fake_data_bamboo) {[{"workEmail" => "email", "employeeId" => "1"}]}
 
-  it "can find a user with their slack_id" do
+  it "it can find a bamboo employeeId with a slack id" do
     user_finder = BambooEmployeeFinder.new(fake_data_slack, fake_data_bamboo)
     expect(user_finder.employee_id("id")).to eq("1")
+  end
+
+  it "it returns user does not exist if the id given is invalid" do
+    user_finder = BambooEmployeeFinder.new(fake_data_slack, fake_data_bamboo)
+    expect(user_finder.employee_id("invalid_id")).to eq("User does not exist in the channel!")
   end
 end
