60a9766b7e263d016af3c3883633029c3185bdd2
Will Curry <watercrafthd@gmail.com>
2017-02-09 11:12:30 +0000

    Clean up EmployeeFinder

--- spec/bamboo_employee_finder_spec.rb
+++ spec/bamboo_employee_finder_spec.rb
@@ -2,10 +2,10 @@ require 'bamboo_employee_finder'
 
 RSpec.describe BambooEmployeeFinder do
   let (:fake_data_slack) {[{user_id: "id", email: "email"}]}
-  let (:fake_data_bamboo) {[{"workEmail" => "email"}]}
+  let (:fake_data_bamboo) {[{"workEmail" => "email", "employeeId" => "1"}]}
 
   it "can find a user with their slack_id" do
     user_finder = BambooEmployeeFinder.new(fake_data_slack, fake_data_bamboo)
-    expect(user_finder.find_employee("id")).to eq(true)
+    expect(user_finder.employee_id("id")).to eq("1")
   end
 end
