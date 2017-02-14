a7a5f9eecf303548a7f9e5cd615a5a9c98f82e64
Will Curry <watercrafthd@gmail.com>
2017-02-14 15:36:52 +0000

    Refactor BamooEmployeeFinder

--- lib/bamboo_employee_finder.rb
+++ lib/bamboo_employee_finder.rb
@@ -13,17 +13,11 @@ class BambooEmployeeFinder
   def employee_data(slack_id, data_wanted)
     @channel_users.each do |user|
       if user.slack_id == slack_id
-        user_data = employee_data_for(user)
-        if user_data
-          return user_data[data_wanted]
-        end
+        employee = @bamboo_employees.find {|employee| have_same_email(user, employee)}
+        return employee[data_wanted] if !employee.nil?
       end
     end
     "User does not exist in the channel!"
-  end
-
-  def employee_data_for(user)
-    @bamboo_employees.find {|employee| have_same_email(user, employee)}
   end
 
   def have_same_email(user, employee)
