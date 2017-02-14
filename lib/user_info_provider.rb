03e12b9f7109f5023131a08981a2bcd48ab781aa
Will Curry <watercrafthd@gmail.com>
2017-02-14 10:01:23 +0000

    Refactor UserInfoProvider

--- lib/user_info_provider.rb
+++ lib/user_info_provider.rb
@@ -4,31 +4,35 @@ require 'json'
 class UserInfoProvider
     def real_name(user_id, team_id)
       token = $teams[team_id][:bot_access_token]
-      real_name = "no user name provided"
-
       url = URI("https://slack.com/api/users.info?token=#{token}&user=#{user_id}&pretty=1")
-
-      begin
-        response = Net::HTTP.get(url)
-        deserialized_response = JSON.parse(response)
-        real_name = deserialized_response['user']['real_name']
-      end
-
-      real_name
+      return get_real_name(url)
     end
 
     def email(user_id, team_id)
       token = $teams[team_id][:bot_access_token]
-      email = "no email provided"
-
       url = URI("https://slack.com/api/users.info?token=#{token}&user=#{user_id}&pretty=1")
+      return get_email(url)
+    end
 
+    private
+
+    def get_real_name(url)
       begin
         response = Net::HTTP.get(url)
         deserialized_response = JSON.parse(response)
-        email = deserialized_response['user']['profile']['email']
+        return deserialized_response['user']['real_name']
+      rescue
+        return "no user name provided"
       end
+    end
 
-      email
+    def get_email(url)
+      begin
+        response = Net::HTTP.get(url)
+        deserialized_response = JSON.parse(response)
+        return deserialized_response['user']['profile']['email']
+      rescue
+        return "no email provided"
+      end
     end
 end
