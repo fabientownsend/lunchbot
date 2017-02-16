7f5ca42f9666ce38a1d4296f50bac29e256ee96b
fabien townsend <fabientownsend@gmail.com>
2017-02-16 17:25:21 +0000

    Clean user info provider rubocop

--- lib/user_info_provider.rb
+++ lib/user_info_provider.rb
@@ -2,9 +2,9 @@ require 'json'
 require 'httparty'
 
 class UserInfoProvider
-    include HTTParty
+  include HTTParty
 
-    base_uri "https://slack.com/api/users.info?token="
+  base_uri "https://slack.com/api/users.info?token="
 
     def real_name(user_id)
       data = user_data(user_id)
@@ -16,14 +16,14 @@ class UserInfoProvider
       data['error'].nil? ? data['user']['email'] : data['error']
     end
 
-    private
+  private
 
-    def user_data(user_id)
-      data = self.class.get("#{token}&user=#{user_id}&pretty=1")
-      JSON.parse(data.body)
-    end
+  def user_data(user_id)
+    data = self.class.get("#{token}&user=#{user_id}&pretty=1")
+    JSON.parse(data.body)
+  end
 
-    def token
-      ENV['BOT_ACCESS_TOKEN']
-    end
+  def token
+    ENV['BOT_ACCESS_TOKEN']
+  end
 end
