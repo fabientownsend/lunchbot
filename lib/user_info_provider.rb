63890361a2d7f538658b73021478dca38bf83152
Will Curry <watercrafthd@gmail.com>
2017-03-03 17:31:31 +0000

    Check for error in user info provider

--- lib/user_info_provider.rb
+++ lib/user_info_provider.rb
@@ -7,11 +7,13 @@ class UserInfoProvider
     base_uri "https://slack.com/api/users.info?token="
 
     def real_name(user_id)
-      user_data(user_id)['user']['real_name']
+      data = user_data(user_id)
+      data['error'].nil? ? data['user']['real_name'] : data['error']
     end
 
     def email(user_id)
-      user_data(user_id)['user']['email']
+      data = user_data(user_id)
+      data['error'].nil? ? data['user']['email'] : data['error']
     end
 
     private
