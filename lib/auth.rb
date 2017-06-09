e62350b4ff915ab97fe8b1785d1fa16762d01128
Will Curry <watercrafthd@gmail.com>
2017-06-09 13:53:01 +0100

    Remove global  variable

--- lib/auth.rb
+++ lib/auth.rb
@@ -17,14 +17,21 @@ end
 
 BOT_SCOPE = 'bot'
 
-$teams = {}
-
 def create_slack_client(slack_api_secret)
   Slack.configure do |config|
     config.token = slack_api_secret
     fail 'Missing API token' unless config.token
   end
   Slack::Web::Client.new
+end
+
+def save_auth_info(response)
+  auth_info = AuthInfo.new(
+      :bot_token => response['bot']['bot_access_token'],
+      :bot_id => response['bot']['bot_user_id'],
+      :user_token => response['access_token']
+      )
+  auth_info.save
 end
 
 class Auth < Sinatra::Base
@@ -56,20 +63,9 @@ class Auth < Sinatra::Base
       )
 
       team_id = response['team_id']
-      $teams[team_id] = {
-        user_access_token: response['access_token'],
-        bot_user_id: response['bot']['bot_user_id'],
-        bot_access_token: response['bot']['bot_access_token']
-      }
 
-      auth_info = AuthInfo.new(
-        :bot_token => response['bot']['bot_access_token'],
-        :bot_id => response['bot']['bot_user_id'],
-        :user_token => response['access_token']
-      )
-      auth_info.save
+      save_auth_info(response)
 
-      $teams[team_id]['client'] = create_slack_client(response['bot']['bot_access_token'])
       status 200
       body "<img src='https://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2016/03/changed-passwords-to-incorrect_admin052413y4ihq.jpg'>"
     rescue Slack::Web::Api::Error => e
