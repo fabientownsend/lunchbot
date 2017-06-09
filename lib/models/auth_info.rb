6de8afa257962e23a9440af73b1d6ec7ef3cd4ff
Will Curry <watercrafthd@gmail.com>
2017-06-09 11:01:11 +0100

    Persist authentication information

---
+++ lib/models/auth_info.rb
@@ -0,0 +1,10 @@
+require 'data_mapper'
+
+class AuthInfo
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :bot_token, String, length: 255
+  property :bot_id, String, length: 255
+  property :user_token, String, length: 255
+end
