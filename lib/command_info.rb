fea07424f97167575c9f93f88837c49a49dd3885
Fabien Townsend <fabientownsend@gmail.com>
2018-04-07 16:51:21 +0100

    Add link to github issue

--- lib/command_info.rb
+++ lib/command_info.rb
@@ -6,36 +6,42 @@ module CommandInfo
   private
 
   def info_array
-    [menu, order, foreman, other, guest]
+    [issues, menu, order, foreman, other, guest]
   end
 
   def menu
-    "To set a menu | `new menu www.menu-url.com`\n" +
+    "To set a menu | `new menu www.menu-url.com`\n" \
     "To get this weeks menu | `menu?`"
   end
 
   def other
-    "To remind everyone who hasn't ordered | `remind`\n" +
+    "To remind everyone who hasn't ordered | `remind`\n" \
     "To mark yourself as out | `out`"
   end
 
   def order
-    "To place an order | `order: food`\n" +
-    "To see someone elses order | `order? @name_of_person`\n" +
-    "To see all orders | `all orders?`\n" +
-    "To see all orders with user ids | `everyone`\n" +
-    "To see sum of orders | `all food orders`\n" +
+    "To place an order | `order: food`\n" \
+    "To see someone elses order | `order? @name_of_person`\n" \
+    "To see all orders | `all orders?`\n" \
+    "To see all orders with user ids | `everyone`\n" \
+    "To see sum of orders | `all food orders`\n" \
     "To copy someones order | `copy order: @username`"
   end
 
   def guest
-    "To add a guest with no order | `add guest: name of guest`\n" +
-    "To remove a guest | `remove guest: name of guest`\n" +
+    "To add a guest with no order | `add guest: name of guest`\n" \
+    "To remove a guest | `remove guest: name of guest`\n" \
     "To place an order for a guest (this also creates a guest if the name given does not exist) | `order -name of guest-: food`"
   end
 
   def foreman
-    "To find out this weeks foreman | `foreman`\n" +
+    "To find out this weeks foreman | `foreman`\n" \
     "To change this weeks foreman to the next in line | `next foreman`"
+  end
+
+  def issues
+    "Want to report a bug :bug: or have an idea for a new feature :package:?\n" \
+    "Share it here: `https://github.com/fabientownsend/lunchbot/issues/new` :loudspeaker:" \
+    "\n\n"
   end
 end
