0a612c4a3b7b4a897b226691fa4860b1524b16d6
Fabien Townsend <fabientownsend@gmail.com>
2018-05-11 16:02:54 +0100

    Adds access for multiple users for one test feature

--- spec/feature_flag_spec.rb
+++ spec/feature_flag_spec.rb
@@ -1,28 +1,61 @@
 class FeatureFlag
-	class << self
-		def test_name(request)
-			@@test_name = request
-		end
-	end
+  @@tester_bucket = {}
 
-	def tester?(request)
-		request == @@test_name
-	end
+  class << self
+    def release_for(*feature_testers)
+      @@tester_bucket[name] = []
+
+      feature_testers.each do |feature_tester|
+        @@tester_bucket[name] << feature_tester
+      end
+    end
+  end
+
+  def feature_access?(request)
+    @@tester_bucket[self.class.name].include?(request)
+  end
 end
 
 class FeatureInProgress < FeatureFlag
-  test_name 'Katerina'
+  release_for 'user 1'
 
-  def applies_to(request)
-		tester?(request[:user_name])
+  def has_access?(request)
+    feature_access?(request[:user_name])
+  end
+end
+
+class SecondFeatureInProgress < FeatureFlag
+  release_for 'user 1', 'user 2'
+
+  def has_access?(request)
+    feature_access?(request[:user_name])
   end
 end
 
 RSpec.describe FeatureFlag do
-	it "returns hello when the person is a tester" do
-		command_example = FeatureInProgress.new
-		slack_data = {user_message: 'user request', user_name: 'Katerina'} 
-		
-    expect(command_example.applies_to(slack_data)).to eq(true)
+  it "returns true when a user has access to a feature" do
+    command_example = FeatureInProgress.new
+
+    slack_data = { user_name: 'user 1' }
+
+    expect(command_example.has_access?(slack_data)).to eq(true)
   end
- end
+
+  it "returns false when a user has not access to a feature" do
+    command_example = FeatureInProgress.new
+
+    slack_data = { user_name: 'user 2' }
+
+    expect(command_example.has_access?(slack_data)).to eq(false)
+  end
+
+  it "multiple testers can have access to one feature" do
+    command_example = SecondFeatureInProgress.new
+
+    user_one = { user_name: 'user 1' }
+    user_two = { user_name: 'user 2' }
+
+    expect(command_example.has_access?(user_one)).to eq(true)
+    expect(command_example.has_access?(user_two)).to eq(true)
+  end
+end
