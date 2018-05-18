caca6305632f105c2d0edb60a0de81615c5f3991
Fabien Townsend <fabientownsend@gmail.com>
2018-05-18 23:58:18 +0100

    Exclude feature flag from Rubocop

--- lib/feature_flag.rb
+++ lib/feature_flag.rb
@@ -1,17 +1,15 @@
 class FeatureFlag
   @@tester_bucket = {}
 
-  class << self
-    def release_for(*feature_testers)
-      @@tester_bucket[name] = []
+  def self.release_for(*feature_testers)
+    @@tester_bucket[name] = []
 
-      feature_testers.each do |feature_tester|
-        @@tester_bucket[name] << feature_tester
-      end
+    feature_testers.each do |feature_tester|
+      @@tester_bucket[name] << feature_tester
     end
   end
 
-  def feature_access?(request)
-    @@tester_bucket[self.class.name].include?(request)
+  def feature_access?(tester)
+    @@tester_bucket[self.class.name].include?(tester)
   end
 end
