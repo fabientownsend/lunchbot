1c2cd13675781453c1ae1ecf33e39af6edd4706b
Fabien Townsend <fabientownsend@gmail.com>
2018-10-17 00:51:30 +0100

    Deploy filtering orders return by crafter office

--- lib/commands/order/get_all_orders_command.rb
+++ lib/commands/order/get_all_orders_command.rb
@@ -1,15 +1,21 @@
-require 'models/order'
-require 'models/crafter'
 require 'days'
+require 'feature_flag'
+require 'models/crafter'
+require 'models/order'
+require 'tiny_logger'
 
 module Commands
-  class GetAllOrders
+  class GetAllOrders < FeatureFlag
+    release_for 'Fabien Townsend'
+
     def applies_to(request)
       request = request[:user_message].downcase
       request == "all orders?"
     end
 
     def prepare(data)
+      @crafter_name = data[:user_name]
+      @crafter = data[:user_id]
     end
 
     def run
@@ -19,7 +25,18 @@ module Commands
     private
 
     def orders
-      orders_of_the_week = Order.all(:date => Days.from_monday_to_friday)
+      orders_of_the_week = []
+
+      if feature_access?(@crafter_name)
+        Logger.info("USE NEW FEATURE ORDER FILTERED")
+        orders_of_the_week = Order.all(:date => Days.from_monday_to_friday).select do |order|
+          Crafter.profile(order.user_id).office == @crafter.office
+        end
+        return orders_of_the_week if orders_of_the_week.empty?
+      else
+        Logger.info("USE OLD FEATURE ORDER FILTERED")
+        orders_of_the_week = Order.all(:date => Days.from_monday_to_friday)
+      end
 
       orders_of_the_week.map do |order|
         name = full_name(order)
