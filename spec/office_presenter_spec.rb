ea3111c19862cab1762456a08c45f5006e823880
Laurent Curau <laurentcurau@gmail.com>
2018-10-22 21:12:31 +0100

    Stub constant

--- spec/office_presenter_spec.rb
+++ spec/office_presenter_spec.rb
@@ -3,7 +3,7 @@ require 'office_presenter'
 RSpec.describe OfficePresenter do
   describe ".locations" do
     it "formats locations" do
-      Office::LOCATIONS  = ["foo", "bar", "baz"]
+      stub_const("Office::LOCATIONS", ["foo", "bar", "baz"])
       expect(OfficePresenter.locations).to eql "Foo, Bar, Baz"
     end
   end
