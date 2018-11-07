d64d86deb31a874fee07c13f2b1381709b207c03
Laurent Curau <laurentcurau@gmail.com>
2018-11-07 22:25:50 +0000

    Inject response into message handler

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -15,12 +15,7 @@ class MessageHandler < FeatureFlag
   def initialize(args = {})
     @mark_all_out = args[:mark_all_out] || MarkAllOut.new
     @request_parser = RequestParser.new
-    if args[:response]
-      @response = args[:response]
-    else
-      @response = Response.new
-      @response.setup
-    end
+    @response = args[:response]
     @user_info = args[:user_info_provider] || UserInfoProvider.new
     @foreman_messager = ForemanMessager.new
     @alert = AlertForeman.new(@foremanMessager)
