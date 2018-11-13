b2d7e3d204b6430bf2febe2879779dbd18e3cda1
Fabien Townsend <fabientownsend@gmail.com>
2018-11-13 22:57:30 +0000

    Update lunch counter to work with office filter (#47) (#63)

--- lib/commands/order/all_food_orders.rb
+++ lib/commands/order/all_food_orders.rb
@@ -8,20 +8,32 @@ module Commands
       "all food orders" == request
     end
 
-    def prepare(data) end
-
-    def run
-      count_meal = Order.aggregate(
-        :lunch,
-        :all.count,
-        date: Days.from_monday_to_friday
-      )
-
-      count_meal.map { |lunch, total| format_text(lunch, total) }.join.strip
+    def prepare(data)
+      @requester = User.profile(data[:user_id])
     end
 
-    def format_text(lunch, total)
-      "#{lunch}: #{total}\n" unless lunch.nil?
+    def run
+      counted_orders.map do |order, count|
+        render_orders_count(order, count)
+      end.join.strip
+    end
+
+    private
+
+    attr_reader :requester
+
+    def counted_orders
+      orders.each_with_object(Hash.new(0)) do |order, results|
+        results[order.lunch] += 1
+      end
+    end
+
+    def orders
+      Order.placed_in(requester.office)
+    end
+
+    def render_orders_count(order, count)
+      "#{order}: #{count}\n" unless order.nil?
     end
   end
 end
--- spec/commands/order/all_food_orders_spec.rb
+++ spec/commands/order/all_food_orders_spec.rb
@@ -17,8 +17,9 @@ RSpec.describe Commands::AllFoodOrders do
     place_order("qwer", "fabien", "burger")
     place_order("poiu", "james", "fish")
 
-    all_food = "burger: 2\nfish: 1"
-    expect(all_food_orders.run).to eq(all_food)
+    all_food_orders.prepare(user_id: "asdf")
+
+    expect(all_food_orders.run).to eq("burger: 2\nfish: 1")
   end
 
   it "return only the meal of the current week" do
@@ -29,23 +30,24 @@ RSpec.describe Commands::AllFoodOrders do
     place_order("qwer", "fabien", "burger")
     place_order("poiu", "james", "fish")
 
-    previous_week_order = Order.create(
+    Order.new(
       user_name: "james",
       lunch: "rice",
       date: previous_week
-    )
-    previous_week_order.save
+    ).save
 
-    all_food = "burger: 2\nfish: 1"
-    expect(all_food_orders.run).to eq(all_food)
+    all_food_orders.prepare(user_id: "asdf")
+
+    expect(all_food_orders.run).to eq("burger: 2\nfish: 1")
   end
 
   it "return monday orders" do
     User.create(user_id: "asdf", user_name: "will", office: "london")
     place_order("asdf", "will", "burger")
 
-    all_food = "burger: 1"
-    expect(all_food_orders.run).to eq(all_food)
+    all_food_orders.prepare(user_id: "asdf")
+
+    expect(all_food_orders.run).to eq("burger: 1")
   end
 
   it "return friday orders" do
@@ -57,8 +59,9 @@ RSpec.describe Commands::AllFoodOrders do
       date: Days.friday
     )
 
-    all_food = "burger: 1"
-    expect(all_food_orders.run).to eq(all_food)
+    all_food_orders.prepare(user_id: "asdf")
+
+    expect(all_food_orders.run).to eq("burger: 1")
   end
 
   it "doesn't count guest without orders" do
@@ -73,8 +76,9 @@ RSpec.describe Commands::AllFoodOrders do
       date: Days.friday
     )
 
-    all_food = "burger: 1"
-    expect(all_food_orders.run).to eq(all_food)
+    all_food_orders.prepare(user_id: "asdf")
+
+    expect(all_food_orders.run).to eq("burger: 1")
   end
 
   private
