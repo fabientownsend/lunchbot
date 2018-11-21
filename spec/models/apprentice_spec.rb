de73c4c8bc28671f28d707f5bd2851fa94f5db1c
Laurent Curau <laurentcurau@gmail.com>
2018-11-21 22:38:15 +0000

    Merge Apprentice class to User

--- spec/models/apprentice_spec.rb
+++ spec/models/apprentice_spec.rb
@@ -1,52 +1,52 @@
-require 'models/apprentice'
+require 'models/user'
 
-RSpec.describe Apprentice do
+RSpec.describe User do
   it "does not set a foreman as current foreman by default" do
-    Apprentice.create(user_id: "the id", office: "london")
+    User.create(user_id: "the id", office: "london")
 
-    apprentice = Apprentice.profile("the id")
+    user = User.profile("the id")
 
-    expect(apprentice.is_foreman).to be false
+    expect(user.is_foreman).to be false
   end
 
   it "sets person as a foreman" do
-    Apprentice.create(user_id: "the id", office: "london")
+    User.create(user_id: "the id", office: "london")
+    User.set_as_foreman("the id", "london")
 
-    Apprentice.set_as_foreman("the id", "london")
-    apprentice = Apprentice.profile("the id")
+    user = User.profile("the id")
 
-    expect(apprentice.is_foreman).to be true
+    expect(user.is_foreman).to be true
   end
 
   it "says if user is a foreman" do
-    Apprentice.create(user_id: "the id", office: "london")
-    Apprentice.set_as_foreman("the id", "london")
+    User.create(user_id: "the id", office: "london")
+    User.set_as_foreman("the id", "london")
 
-    expect(Apprentice.foreman?("the id")).to be true
+    expect(User.foreman?("the id")).to be true
   end
 
   it "says if user is not a foreman" do
-    Apprentice.create(user_id: "the id", office: "london")
+    User.create(user_id: "the id", office: "london")
 
-    expect(Apprentice.foreman?("the id")).to be false
+    expect(User.foreman?("the id")).to be false
   end
 
   it "removes previous foreman if one was found" do
-    Apprentice.create(user_id: "Margarette", office: "london")
-    Apprentice.create(user_id: "Claudia", office: "london")
+    User.create(user_id: "Margarette", office: "london")
+    User.create(user_id: "Claudia", office: "london")
 
-    Apprentice.set_as_foreman("Margarette", "london")
-    apprentice_one = Apprentice.profile("Margarette")
-    apprentice_two = Apprentice.profile("Claudia")
+    User.set_as_foreman("Margarette", "london")
+    user_one = User.profile("Margarette")
+    user_two = User.profile("Claudia")
 
-    expect(apprentice_one.is_foreman).to be true
-    expect(apprentice_two.is_foreman).to be false
+    expect(user_one.is_foreman).to be true
+    expect(user_two.is_foreman).to be false
 
-    Apprentice.set_as_foreman("Claudia", "london")
-    apprentice_one = Apprentice.profile("Margarette")
-    apprentice_two = Apprentice.profile("Claudia")
+    User.set_as_foreman("Claudia", "london")
+    user_one = User.profile("Margarette")
+    user_two = User.profile("Claudia")
 
-    expect(apprentice_one.is_foreman).to be false
-    expect(apprentice_two.is_foreman).to be true
+    expect(user_one.is_foreman).to be false
+    expect(user_two.is_foreman).to be true
   end
 end
