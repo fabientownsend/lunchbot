9e52a89826ebe0f2a41824043457bfdafe18ba57
Fabien Townsend <fabientownsend@gmail.com>
2019-01-09 21:54:29 +0000

    MOves orders of verification in command handler

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -15,20 +15,20 @@ class MessageHandler
   end
 
   def handle(requester)
-    requester.message
     return unless requester.has_message?
 
-    data = format_data(requester)
-    returned_command = @request_parser.parse(data)
+    formated_data = format(requester)
 
     unless User.profile(requester.id)
-      User.create(data)
+      User.create(formated_data)
     end
 
-    if !User.has_office?(requester.id) && !Commands::AddOffice.add_office_request?(data)
+    if !User.has_office?(requester.id) && !Commands::AddOffice.add_office_request?(formated_data)
       @bot.send("You need to add your office. ex: \"office: london\"", requester.recipient)
       return
     end
+
+    returned_command = @request_parser.parse(formated_data)
 
     unless returned_command.nil?
       response = deal_with_command(returned_command)
@@ -47,7 +47,7 @@ class MessageHandler
     response
   end
 
-  def format_data(requester)
+  def format(requester)
     {
       user_message: requester.message,
       user_id: requester.id,
