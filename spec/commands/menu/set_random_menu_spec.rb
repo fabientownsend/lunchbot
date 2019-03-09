91fc3e965006b6b0b03b34db6af4e50045f760c2
Himalee <himalee.tailor@gmail.com>
2019-03-09 21:39:33 +0000

    Sets menu randomly from past menus

--- spec/commands/menu/set_random_menu_spec.rb
+++ spec/commands/menu/set_random_menu_spec.rb
@@ -6,4 +6,41 @@ RSpec.describe Commands::SetRandomMenu do
     response = command.applies_to?(user_message: "surprise menu")
     expect(response).to be true
   end
+
+  it "returns a random menu" do
+    User.new(
+      user_name: "Fabien Townsend",
+      slack_id: "valid id",
+      office: "london",
+      is_foreman: true
+    ).save
+
+    command = Commands::SetRandomMenu.new
+    data = { user_id: "valid id" }
+
+    allow(command).to receive(:random_url).and_return("foo.com")
+
+    command.prepare(data)
+
+    expect(command.run).to include("foo.com")
+  end
+
+  it "returns a random menu URL from past orders" do
+    Menu.new(url: "foo", office: "Winterfell").save
+    Menu.new(url: "bar", office: "Winterfell").save
+
+    User.new(
+      user_name: "Fabien Townsend",
+      slack_id: "valid id",
+      office: "Winterfell",
+      is_foreman: true
+    ).save
+
+    command = Commands::SetRandomMenu.new
+    data = { user_id: "valid id" }
+
+    command.prepare(data)
+
+    expect(command.run).to include("foo").or(include("bar"))
+  end
 end
